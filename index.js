4e89854ee3f70b772915cf6d24371c5cabf5f9c7
nikson-jekson <kuleshov.nikolay@gmail.com>
2016-08-21 03:56:55 +0300

    test commit

--- index.js
+++ index.js
@@ -12,6 +12,7 @@
    OF ANY KIND, either express or implied. See the License for the
    specific language governing permissions and limitations under the
    License.
+
  */
 var express = require('express');
 var app = express();
